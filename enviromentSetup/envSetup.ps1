6f03255ddef5a8fa6226f35668028ad26966a16d
TeejMcSteez <tjhall047@gmail.com>
2025-05-27 14:02:29 -0400

    Added enviroment setup and a script to help

---
+++ enviromentSetup/envSetup.ps1
@@ -0,0 +1,20 @@
+# Using aliases so Linux users are chill
+echo "Make sure this is the directory the project is in!"
+
+echo "Creating venv folder . . ."
+
+python -m venv venv
+
+echo "Activating Enviroment . . ."
+
+"Invoke-Expression './venv/Scripts/Activate.ps1'" > "./activate.ps1"
+
+./activate.ps1
+
+echo "Run activate to setup venv for later use"
+
+"Invoke-Expression 'Remove-Item -Path .\venv, .\activate.ps1, envSetup.ps1 -Recurse -Force'" > "./remove.ps1"
+
+echo "Run remove.ps1 to remove enviroment and then delete remove script to clean . . ."
+
+echo "Script activated ready to go!"
---
+++ enviromentSetup/test.py
@@ -0,0 +1,1 @@
+print("Welcome TJ to the Software Engineering course!")
