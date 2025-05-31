f3a1da575361e0e8e53bb0212bd51a87e0e1a5a2
Teej <107083710+TeejMcSteez@users.noreply.github.com>
2025-05-31 13:32:23 -0400

    Update envSetup.ps1

--- envSetup.ps1
+++ envSetup.ps1
@@ -6,13 +6,13 @@ echo "Creating venv folder . . ."
 python -m venv venv
 
 echo "Activating Enviroment . . ."
-
+# Activate script wont work on linux
 "Invoke-Expression './venv/Scripts/Activate.ps1'" > "./activate.ps1"
 
 ./activate.ps1
 
 echo "Run activate to setup venv for later use"
-
+# Remove script wont work on linux
 "Invoke-Expression 'Remove-Item -Path .\venv, .\activate.ps1, envSetup.ps1 -Recurse -Force'" > "./remove.ps1"
 
 echo "Run remove.ps1 to remove enviroment and then delete remove script to clean . . ."
